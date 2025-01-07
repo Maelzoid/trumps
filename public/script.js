00666b35deb5bb5f1f7cb72abe2aba5225ae1d37
Maelzoid <jmaelzer@yahoo.co.uk>
2025-01-07 11:46:31 +0000

    update to functions etc.

--- public/script.js
+++ public/script.js
@@ -432,6 +432,27 @@ window.onload = function() {
     fetchPlayerList();
 };
 
+function preloadImages(folder) {
+    const images=['A1','A2','A3','A4','B1','B2','B3','B4','C1','C2','C3','C4','D1','D2','D3','D4','E1','E2','E3','E4','F1','F2','F3','F4','G1','G2','G3','G4','H1','H2','H3','H4','I1','I2','I3','I4','J1','J2','J3','J4','K1','K2','K3','K4','L1','L2','L3','L4','M1','M2','M3','M4']
+    const preloadedImages = [];
+    
+    images.forEach((imageName) => {
+        const img = new Image();
+        img.onerror = () => {
+            // Silently handle missing images; no logging needed
+            img.src = ""; // Optional: Set to an empty string to ensure no broken references
+        };
+        img.src = `images/${folder}/${imageName}.jpg`;
+        preloadedImages.push(img);
+    });
+    
+        return preloadedImages; // Optional: Access preloaded images later if needed
+    }
+
+
+
+
+
 
 function goToStartGame () {
     hostName.value = thisPlayer
@@ -496,6 +517,7 @@ function goToLobby() {
     joinGame.style.display = "none";
     lobby.style.display = "block";
     window.scrollTo({ top: 0 });
+    preloadImages(thisDeck)
     
 }
 
